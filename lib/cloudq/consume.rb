25d528ce12840de6ef8066eaf122de781433bd62
Nathan Hall <nathan.mh@gmail.com>
2017-03-22 15:47:37 -0400

    updated restclient.get

--- lib/cloudq/consume.rb
+++ lib/cloudq/consume.rb
@@ -14,7 +14,7 @@ module Cloudq
     end
 
     def get(&block)
-      resp = RestClient.get(:url => url, {:timeout => 300, :open_timeout => 300})
+      resp = RestClient.get(url, {:timeout => 300, :open_timeout => 300})
       if resp.code == 200
         result = JSON.parse(resp)
         return nil if result['status'] == 'empty'
